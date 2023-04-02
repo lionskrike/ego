fd2bff17f68fc70e102cb814824990eedbe2fda8
Robin <greerrobin47@gmail.com>
2023-04-01 23:09:39 -0500

    container

---
+++ src/app/components/button-container/button-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-container',
+  templateUrl: './button-container.component.html',
+  styleUrls: ['./button-container.component.scss']
+})
+export class ButtonContainerComponent {
+  title = 'ego';
+}

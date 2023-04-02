294ae63286c9c0b409f420d300c7ca0a6f852712
Robin <greerrobin47@gmail.com>
2023-04-02 00:28:03 -0500

    added content column proto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonContainerComponent } from './components/button-container/button-container.component';
+import { ContentColumnComponent } from './content-column/content-column.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonContainerComponent
+    ButtonContainerComponent,
+    ContentColumnComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content-column/content-column.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-column',
+  templateUrl: './content-column.component.html',
+  styleUrls: ['./content-column.component.scss']
+})
+export class ContentColumnComponent {
+}
